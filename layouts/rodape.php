ce3a1a86cc9696517a9de247a2d48473f1c3a43e
cristiancdias <cristiancdias@gmail.com>
2020-12-12 23:30:15 -0300

    alterando rodape para php

---
+++ layouts/rodape.php
@@ -0,0 +1,72 @@
+<div class="container-fluid rodape py-5"> <!--Rodapé-->
+      <div class="container">
+        <div class="row">
+          <div class="col-md-9">
+            <div class="row">
+              <div class="col-md-2">
+                <img src="./imagens/spotify-logo-branca-white.png" 
+                  alt="Spotify" class="logo" 
+                >
+              </div>
+              <div class="col-md-3">
+                <h6>EMPRESA</h6>
+                <ul>
+                  <li><a href="#">Sobre</a></li>
+                  <li><a href="#">Empregos</a></li>
+                  <li><a href="#">For the Record</a></li>
+                </ul>
+              </div>
+              <div class="col-md-3">
+                <h6>COMUNIDADES</h6>
+                <ul>
+                  <li><a href="#">Para Artistas</a></li>
+                  <li><a href="#">Desenvolvedores</a></li>
+                  <li><a href="#">Publicidade</a></li>
+                  <li><a href="#">Investidores</a></li>
+                  <li><a href="#">Fornecedores</a></li>
+                </ul>
+              </div>
+              <div class="col-md-4">
+                <h6>LINKS ÚTEIS</h6>
+                <ul>
+                  <li><a href="#">Suporte</a></li>
+                  <li><a href="#">Player da Web</a></li>
+                  <li><a href="#">Aplicativo móvel grátis</a></li>
+                  <li><a href="#">Retrospectiva 2020</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="row">
+              <div class="col-md-4">
+                <a href="#"><img src="./imagens/instagram.png" alt="Instagram"></a>
+              </div>
+              <div class="col-md-4">
+                <a href="#"><img src="./imagens/twitter.png" alt="Twitter"></a>
+              </div>
+              <div class="col-md-4">
+                <a href="#"><img src="./imagens/facebook.png" alt="Facebook"></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-11"></div>
+          <div class="col-md-1 brasil">Brasil <img src="./imagens/Brasil.png" alt="Brasil"></div>
+        </div>
+        <div class="row footers">
+          <div class="col-md-10">
+          <a href="#">Legal</a>
+          <a href="#">Centro de Privacidade</a>
+          <a href="#">Politica de Privacidade</a>
+          <a href="#">Cookies</a>
+          <a href="#">Sobre anúncios</a></div>
+          <div class="col-md-2 copyright"> &copy 2020 Spotify AB</div>
+        </div>
+      </div>
+    </div>
+
+    <script src="./library/bootstrap/js/bootstrap.bundle.min.js"> </script>
+  </body>
+</html>
