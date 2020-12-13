13d678ff88b62a07fdfe185a1cd1a713d66d1830
cristiancdias <cristiancdias@gmail.com>
2020-12-12 23:31:41 -0300

    alterando cabecalho para php

---
+++ layouts/cabecalho.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./library/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+
+    <title>Spotify</title>
+  </head>
+  <body>
+    <div class="container-fluid py-3 navegacao-superior"><!-- Navegação Superior-->
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <img src="./imagens/spotify-logo-branca-white.png" alt="logo" class="logo">
+          </div>
+          <div class="col-md-4">
+            <nav class="navbar navbar-expand-lg">
+              <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                  <ul class="navbar-nav">
+                    <li class="nav-item">
+                      <a class="nav-link" aria-current="page" href="#">
+                        <strong>Premium</strong>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">
+                        <strong>Suporte</strong>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">
+                        <strong>Baixar</strong>
+                      </a>
+                    </li>
+                    <span>|</span>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-expanded="false">
+                        <img src="./icones/account_circle-24px.svg" alt="Foto" class="icone-usuario-pequeno">
+                        <strong>Perfil</strong>
+                      </a>
+                      <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li>
+                          <a class="dropdown-item" href="#">
+                            <strong>Conta</strong>
+                          </a>
+                        </li>
+                        <li>
+                          <a class="dropdown-item sair" href="#">
+                            <strong>Sair</strong>
+                          </a>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
