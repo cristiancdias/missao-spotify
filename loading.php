9e0621a5688f711bb6548dd981d49c257346a29a
cristiancdias <cristiancdias@gmail.com>
2020-12-15 00:15:29 -0300

    script de validaçao de idade e tela de loading

---
+++ loading.php
@@ -0,0 +1,12 @@
+
+<?php
+      $ano = $_POST['validacao'];
+      $soma = 2020 - $ano;
+      if ($soma <= 14){
+        echo "<script type='text/javascript'>alert('Você não pode alterar o cadastro idade inferior a permitida!');javascript:window.location='editar-perfil.php';</script>";
+      } else {
+        echo "<body style='background-color: #000;'>";
+          echo "<img src='./imagens/loading.jpg' style='margin-left: 400px;'>";
+        echo "</body>";
+      }
+?>
