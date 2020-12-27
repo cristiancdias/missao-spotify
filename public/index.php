fd183b3fa5460c3e7bbe3b394207bf60cb0f29f4
cristiancdias <cristiancdias@gmail.com>
2020-12-27 20:33:19 -0300

    estruturacao da pagina index

--- public/index.php
+++ public/index.php
@@ -2,7 +2,63 @@
   include __DIR__ . "/../layouts/cabecalho.php";
 ?>
 
-
+<div class="container-fluid">
+  <div class="container py-5">
+    <div class="row">
+      <div class="col-md-8">
+        <h4>SPOTIFY PREMIUM</h4>
+        <h1>Tá acabando: 3 meses de Premium grátis</h1>
+        <h6>Oferta acaba em 5 dias.</h6>
+        <h6>
+          Não perca a chance de ouvir sua música sem anúncios, no modo offline e 
+          muito mais. Cancele quando quiser.
+        </h6>
+        <a href="#" class="btn btn-primary">GANHE 3 MESES GRÁTIS</a>
+        <p>
+          Somente para o plano Individual. Depois, apenas R$ 16,90/mês. 
+          Oferta indisponível para usuários que já experimentaram o Premium. 
+          <a href="#" alt="link">Sujeita a Termos e Condições</a>. 
+          A oferta termina em 31 de dez de 2020.
+        </p>
+      </div>
+      <div class="col-md-4">
+        <img class="img-fluid" src="./imagens/disco.png" alt="disco">
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container-fluid">
+  <div class="container py-5">
+    <div class="row">
+      <div class="col-md-6">
+        <img src="./imagens/retrospectiva.png" alt="retro" class="img-fluid">
+      </div>
+      <div class="col-md-6">
+        <h4>RETROSPECTIVA 2020</h4>
+        <h2>Veja o que rolou no seu Spotify</h2>
+        <h6>
+          Descubra os artistas, músicas e podcasts que te ajudaram a sobreviver
+          ao ano mais longo da história.
+        </h6>
+        <a href="#" class="btn btn-primary">CONFIRA SUA RETROSPECTIVA</a>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container-fluid">
+  <div class="container py-5">
+    <div class="row">
+      <div class="col-md-5">
+        <h2>Escutar muda tudo</h2>
+        <h6>
+          Milhões de músicas e podcasts para explorar. E nem precisa de cartão
+          de crédito
+        </h6>
+        <a href="#" class="btn btn-primary">OBTENHA O SPOTIFY FREE</a>
+      </div>
+    </div>
+  </div>
+</div>
 <?php
   include __DIR__ . "/../layouts/rodape.php";
 ?>
