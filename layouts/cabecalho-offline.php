aef948c14fb64458e762bc9b787436e59827ad95
cristiancdias <cristiancdias@gmail.com>
2020-12-27 23:15:17 -0300

    criando um cabecalho offline

---
+++ layouts/cabecalho-offline.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./library/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Spotify</title>
+  </head>
+  <body>
+    <div class="container-fluid py-3 navegacao-superior"><!-- Navegação Superior-->
+      <nav class="navbar navbar-expand-lg">
+        <div class="container">
+          <a class="navbar-brand" href="index.php">
+            <img src="./imagens/spotify-logo-branca-white.png" alt="logo" class="logo">
+          </a>
+          <div class="collapse navbar-collapse d-flex flex-row-reverse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">
+                  <strong>Premium</strong>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <strong>Suporte</strong>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <strong>Baixar</strong>
+                </a>
+              </li>
+              <li>
+                <span>|</span>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <strong>Inscrever-se</strong>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="home.php">
+                  <strong>Entrar</strong>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
