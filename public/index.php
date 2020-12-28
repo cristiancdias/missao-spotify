18c949e7772d31f204f2fc062e533309be15f558
cristiancdias <cristiancdias@gmail.com>
2020-12-27 21:58:08 -0300

    terminando versão 1 do index

--- public/index.php
+++ public/index.php
@@ -2,14 +2,14 @@
   include __DIR__ . "/../layouts/cabecalho.php";
 ?>
 
-<div class="container-fluid">
+<div class="container-fluid heading">
   <div class="container py-5">
     <div class="row">
-      <div class="col-md-8">
+      <div class="col-md-6">
         <h4>SPOTIFY PREMIUM</h4>
-        <h1>Tá acabando: 3 meses de Premium grátis</h1>
-        <h6>Oferta acaba em 5 dias.</h6>
-        <h6>
+        <h1 class="pt-3">Tá acabando: 3 meses de Premium grátis</h1>
+        <h6 class="mt-5 pt-5">Oferta acaba em 5 dias.</h6>
+        <h6 class="pb-4">
           Não perca a chance de ouvir sua música sem anúncios, no modo offline e 
           muito mais. Cancele quando quiser.
         </h6>
@@ -21,13 +21,13 @@
           A oferta termina em 31 de dez de 2020.
         </p>
       </div>
-      <div class="col-md-4">
+      <div class="col-md-5 offset-md-1">
         <img class="img-fluid" src="./imagens/disco.png" alt="disco">
       </div>
     </div>
   </div>
 </div>
-<div class="container-fluid">
+<div class="container-fluid heading inverter py-5">
   <div class="container py-5">
     <div class="row">
       <div class="col-md-6">
@@ -35,26 +35,27 @@
       </div>
       <div class="col-md-6">
         <h4>RETROSPECTIVA 2020</h4>
-        <h2>Veja o que rolou no seu Spotify</h2>
-        <h6>
+        <h2 class="py-3">Veja o que rolou no seu Spotify</h2>
+        <h6 class="pb-3">
           Descubra os artistas, músicas e podcasts que te ajudaram a sobreviver
           ao ano mais longo da história.
         </h6>
-        <a href="#" class="btn btn-primary">CONFIRA SUA RETROSPECTIVA</a>
+        <a href="#" class="btn btn-primary btn-inverter">CONFIRA SUA RETROSPECTIVA</a>
       </div>
     </div>
   </div>
 </div>
-<div class="container-fluid">
+<div class="container-fluid inverter banner">
   <div class="container py-5">
     <div class="row">
       <div class="col-md-5">
+        <h4 class="py-4">SPOTIFY FREE</h4>
         <h2>Escutar muda tudo</h2>
-        <h6>
+        <h6 class="pb-4">
           Milhões de músicas e podcasts para explorar. E nem precisa de cartão
           de crédito
         </h6>
-        <a href="#" class="btn btn-primary">OBTENHA O SPOTIFY FREE</a>
+        <a href="#" class="btn btn-primary btn-banner">OBTENHA O SPOTIFY FREE</a>
       </div>
     </div>
   </div>
